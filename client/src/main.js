a194e602b904dae55e06c2cadc0e6954e28c89cc
swapnilbhadoriya10@gmail.com <swapnilbhadoriya10@gmail.com>
2023-03-06 17:15:51 +0530

    Added bootstrap and vue router

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router' 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
